a122039c2c57bda5b63df0644a280e745e742807
CEF2003 <124346261+CEF2003@users.noreply.github.com>
2024-05-13 21:46:35 +0300

    Added radix sort and counting sort methods

---
+++ DisjointSets/disjoint_sets.py
@@ -0,0 +1,20 @@
+class disjoint_sets:
+    def __init__(self, nb_of_items):
+        self.ens = []
+        for i in range(0,nb_of_items):
+            self.ens.append(-1)
+
+    def fin_rec(self, x):
+        if self.ens[x] < 0:
+            return x
+        else:
+            return self.find_rec(self.ens[x])
+
+    def find(self, x):
+        while self.ens[x] > 0:
+            x = self.ens[x]
+        return x
+
+    def union(self, a, b):
+        self.ens[b] = a
+
